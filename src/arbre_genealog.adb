06941aa53c1c0c1b4f23fd01362b85047ab37be8
CelianHache <celian.hache@gmail.com>
2025-01-25 19:55:05 +0100

    [Spec_Genealog] Add refining in adb

--- src/arbre_genealog.adb
+++ src/arbre_genealog.adb
@@ -74,14 +74,20 @@ package body Arbre_Genealog is
       Base_Id: String := Get_Id(Tree);
       Path: String := Id_Child(Base_Id'Length + 1 .. Id_Child'Last);
    begin
+   -- R0 - Get a node based on its ID
+      -- R1 - Retrieve the direct ancestor
       Temp_Tree := Tree;
       for i in Path'Range loop
+         -- R2 -Check the digits of the searched id
          if Path(i) = '1' then
+            -- R3 - Replace current tree by the father's tree
             Temp_Tree := Get_Father(Temp_Tree);
          elsif Path(i) = '2' then
+            -- R3 - Replace current tree by the father's tree
             Temp_Tree := Get_Mother(Temp_Tree);
          end if;
       end loop;
+      -- R1 - Return the tree
       return Temp_Tree;
    end Get_Node_By_Id;
 
@@ -91,14 +97,19 @@ package body Arbre_Genealog is
       Father_Ancestors : Ancestor_Array(1 .. 2**(Generation-1));  -- Taille initiale
       Count : Integer := 0;
    begin
+   -- R0 - Retrieve ancestors of a specified generation
+      -- R1 - Check the generation 
       if Generation = 0 then 
+         -- R2 - Return the value of the current node
          if (not Is_Null (Tree)) then 
             Ancestors(1) := Get_Value(Tree);
          end if;
          return Ancestors;
       else 
+         -- R2 - Get ancestors from previous generations
          Mother_Ancestors := Get_Ancestors_Generation (Get_Mother(Tree), Generation -1);
          Father_Ancestors := Get_Ancestors_Generation (Get_Father(Tree), Generation -1);
+         -- R2 - Copy previous ancestors with the current ones
          for i in Mother_Ancestors'Range loop 
             Ancestors(i) := Mother_Ancestors(i);
             Count := Count + 1;
@@ -106,6 +117,7 @@ package body Arbre_Genealog is
          for i in Father_Ancestors'Range loop
             Ancestors(Count + i) := Father_Ancestors(i);
          end loop;
+         -- R2 - Return ancestors
          return Ancestors;
       end if;
    end Get_Ancestors_Generation;
@@ -114,19 +126,23 @@ package body Arbre_Genealog is
       Temp : T_Personne;
       New_Array : Ancestor_Array(List'Range);
    begin
+   -- R0 - Sort the ancestors array
+      -- R1 - Copy the list
       New_Array := List;
+      -- R1 - Sorted the copied list
       for I in New_Array'First .. New_Array'Last - 1 loop
          for J in I + 1 .. New_Array'Last loop
-            -- Comparer d'abord les noms, puis les prénoms, en déréférant les pointeurs
+            -- R2 - Compare names and first names
             if (Get_Name(New_Array(I)).all > Get_Name(New_Array(J)).all) or
                (Get_Name(New_Array(I)).all = Get_Name(New_Array(J)).all and then Get_First_Name(New_Array(I)).all > Get_First_Name(New_Array(J)).all) then
-               -- Échanger les éléments si la condition est vraie
+               -- R3 - Echange values
                Temp := New_Array(I);
                New_Array(I) := New_Array(J);
                New_Array(J) := Temp;
                end if;
          end loop;
       end loop;
+      -- R1 - Return array
       return New_Array;
    end Get_Sorted_Ancestor_Array;
 
@@ -134,101 +150,115 @@ package body Arbre_Genealog is
       Sorted_Array_1 : Ancestor_Array(Array1'Range);
       Sorted_Array_2 : Ancestor_Array(Array2'Range);
    begin
+   -- R0 - Eval if the contents of two lists are equals
+      -- R1 - Sort the lists
       Sorted_Array_1 := Get_Sorted_Ancestor_Array(Array1);
       Sorted_Array_2 := Get_Sorted_Ancestor_Array(Array2);
+      -- R1 - Compare the lists
       for i in Sorted_Array_1'Range loop
+         -- R2 - Check the equality
          if Sorted_Array_1(i) /= Sorted_Array_2(i) 
             and Get_Name(Sorted_Array_1(i)).all /= "" 
             and Get_Name (Sorted_Array_2(i)).all /= ""
             and Get_First_Name(Sorted_Array_1(i)).all /= "" 
             and Get_First_Name (Sorted_Array_2(i)).all /= ""
          then
+         -- R2 - Returns false when the lists are different
             return false;
          end if;
       end loop;
+      -- R1 - Return true if the lists are equals
       return true;
    end Equals;
 
-function Count_Ancestors(Tree: in T_Arbre_Personnes; Id_Node : in String) return Integer is
-   Root, Left, Right : T_Arbre_Personnes;
-   Ancestors_Left, Ancestors_Right : Integer;
-begin
-
-   Root := Get_Node_By_Id(Tree, Id_Node);
-
-   if Is_Null(Root) then
-      return 0;
-   end if;
-
-   Left := Get_Left(Root);
-   Right := Get_Right(Root);
-
-   Ancestors_Left := 0;
-   Ancestors_Right := 0;
-
-   if not Is_Null(Left) then
-      Ancestors_Left := Count_Ancestors(Tree, Get_Id(Left));
-   end if;
-
-   if not Is_Null(Right) then
-      Ancestors_Right := Count_Ancestors(Tree, Get_Id(Right));
-   end if;
-
-   -- Retourner la somme des ancêtres, en incluant le nœud courant
-   return 1 + Ancestors_Left + Ancestors_Right;
-end Count_Ancestors;
-
-procedure Remove_Family_Member(Tree : in out T_Arbre_Personnes; Id_Node : in String) is
-   Root, Child : T_Arbre_Personnes;
-   Value : T_Personne;
-begin
-   -- Supprimer la référence du noeud par l'enfant si différent de la racine
-   if (Id_Node'Length >= 2) then
+   function Count_Ancestors(Tree: in T_Arbre_Personnes; Id_Node : in String) return Integer is
+      Root, Left, Right : T_Arbre_Personnes;
+      Ancestors_Left, Ancestors_Right : Integer;
+   begin
+   -- R0 - Count the ancestors
+      -- R1 - Retrieve root
       Root := Get_Node_By_Id(Tree, Id_Node);
-      Child := Get_Child(Tree, Id_Node);
-      if Is_Father(Id_Node) then
-         Remove_Father(Child);
-      else
-         Remove_Mother(Child);
+      -- R1 - Return 0 if root is null
+      if Is_Null(Root) then
+         return 0;
       end if;
-   else 
-      -- Libérer les champs dynamiques dans la valeur `T_Personne` du nœud
-      Remove(Tree);
-   end if;
+      -- R1 - Retrieve left parent
+      Left := Get_Left(Root);
+      -- R1 - Retrieve right parent
+      Right := Get_Right(Root);
+      -- R1 - Count number of ancestors for the parents
+         -- R2 - Initialise the count 
+      Ancestors_Left := 0;
+      Ancestors_Right := 0;
+         -- R2 - Count the ancestors for the left parent
+      if not Is_Null(Left) then
+         Ancestors_Left := Count_Ancestors(Tree, Get_Id(Left));
+      end if;
+         -- R2 - Count the ancestors for the right parent
+      if not Is_Null(Right) then
+         Ancestors_Right := Count_Ancestors(Tree, Get_Id(Right));
+      end if;
 
-end Remove_Family_Member;
+      -- R1 - Return the sum of the ancestors
+      return 1 + Ancestors_Left + Ancestors_Right;
+   end Count_Ancestors;
 
-procedure Remove_Father(Child: in out T_Arbre_Personnes) is
-   Father_Personne : T_Personne;
-begin
-   Remove_Left(Child);
-end Remove_Father;
+   procedure Remove_Family_Member(Tree : in out T_Arbre_Personnes; Id_Node : in String) is
+      Root, Child : T_Arbre_Personnes;
+      Value : T_Personne;
+   begin
+   -- R0 - Remove a family member and all its ancestors
+      if (Id_Node'Length >= 2) then
+         -- R1 - Retrieve the node
+         Root := Get_Node_By_Id(Tree, Id_Node);
+         -- R1 - Retrieve the child of the node
+         Child := Get_Child(Tree, Id_Node);
+         -- R1 - Remove the parent
+         if Is_Father(Id_Node) then
+            -- R2 - Remove the father 
+            Remove_Father(Child);
+         else
+            -- R2 - Remove the mother
+            Remove_Mother(Child);
+         end if;
+      else 
+         -- R1 - Free the node and all the values in T_Personne
+         Remove(Tree);
+      end if;
 
-procedure Remove_Mother(Child: in out T_Arbre_Personnes) is
-begin
-   Remove_Right(Child);
-end Remove_Mother;
+   end Remove_Family_Member;
 
-function Get_Child(Tree : in T_Arbre_Personnes; Id_Node : in String) return T_Arbre_Personnes is
-   Child: T_Arbre_Personnes;
-   Id_Child : String(1..Id_Node'Last-1); 
-begin 
-   Id_Child := Id_Node(Id_Node'First..Id_Node'Last - 1);
-   Child := Get_Node_By_Id(Tree, Id_Child); 
-   return Child;
-end Get_Child;
+   procedure Remove_Father(Child: in out T_Arbre_Personnes) is
+      Father_Personne : T_Personne;
+   begin
+      Remove_Left(Child);
+   end Remove_Father;
 
-function Is_Father(Id_Node : in String) return Boolean is
-   Last_Char: Character;
-begin
-   Last_Char := Id_Node(Id_Node'Last);
+   procedure Remove_Mother(Child: in out T_Arbre_Personnes) is
+   begin
+      Remove_Right(Child);
+   end Remove_Mother;
 
-   if Last_Char = '1' then
-      return True;
-   end if;
+   function Get_Child(Tree : in T_Arbre_Personnes; Id_Node : in String) return T_Arbre_Personnes is
+      Child: T_Arbre_Personnes;
+      Id_Child : String(1..Id_Node'Last-1); 
+   begin 
+      Id_Child := Id_Node(Id_Node'First..Id_Node'Last - 1);
+      Child := Get_Node_By_Id(Tree, Id_Child); 
+      return Child;
+   end Get_Child;
 
-   return False;
-end Is_Father;
+   function Is_Father(Id_Node : in String) return Boolean is
+      Last_Char: Character;
+   begin
+      Last_Char := Id_Node(Id_Node'Last);
+
+      if Last_Char = '1' then
+         return True;
+      end if;
+
+      return False;
+   end Is_Father;
 
    function Get_Tree_Depth(Tree: in T_Arbre_Personnes) return Integer is
    begin
@@ -275,14 +305,19 @@ end Is_Father;
          Count : Integer := 0;
          Padding : Integer := 0;
       begin 
+      -- R0 - Get the nodes with both parents
+         -- R1 - Check for ancestors
          if not Is_Null (Tree) then
+            -- R2 - Add the ancestor if it has two parents
             if Has_Two_Parents (Tree) then
                Ancestors(1) := Get_Value(Tree);
                Padding := 1;
             end if;
+            -- R2 - Get the ancestors of the mother and the father
             Mother := Get_Mother(Tree);
             Father := Get_Father(Tree);
             if not Is_Null (Mother) then 
+               -- R3 - Get the node with both parents in the mother tree 
                Mother_Ancestors := Nodes_With_Two_Parents_Generation(Mother, Depth-1);
                for i in Mother_Ancestors'Range loop 
                   Ancestors(i + Padding) := Mother_Ancestors(i);
@@ -290,12 +325,14 @@ end Is_Father;
                end loop;
             end if;
             if not Is_Null (Father) then
+               -- R3 - Get the node with both parents in the father tree
                Father_Ancestors := Nodes_With_Two_Parents_Generation(Father, Depth-1);
                for i in Father_Ancestors'Range loop
                   Ancestors(Count + i) := Father_Ancestors(i);
                end loop;
             end if;
          end if;
+         -- R1 - Return the ancestors
          return Ancestors;
       end Nodes_With_Two_Parents_Generation;
    begin
@@ -310,24 +347,31 @@ end Is_Father;
          Father : T_Arbre_Personnes;
          Mother : T_Arbre_Personnes;
       begin 
+      -- R0 - Get the nodes with only one parent
+         -- R1 - Check for ancestors
          if not Is_Null (Tree) then
+            -- R2 - Add the ancestor if it has only one parent
             if Has_Only_One_Parent (Tree) then
                Ancestors(1) := Get_Value(Tree);
             end if;
+            -- R2 - Get the ancestors of the mother and the father
             Mother := Get_Mother(Tree);
             Father := Get_Father(Tree);
             if not Is_Null (Mother) then 
+               -- R3 - Get the node with only one parent in the mother tree 
                Mother_Ancestors := Nodes_With_Only_One_Parent_Generation(Mother, Depth-1);
                for i in Mother_Ancestors'Range loop 
                   Ancestors(i + 1) := Mother_Ancestors(i);
                end loop;
             else
+               -- R3 - Get the node with only one parent in the father tree 
                Father_Ancestors := Nodes_With_Only_One_Parent_Generation(Father, Depth-1);
                for i in Father_Ancestors'Range loop
                   Ancestors(i + 1) := Father_Ancestors(i);
                end loop;
             end if;
          end if;
+         -- R1 - Return the ancestors
          return Ancestors;
       end Nodes_With_Only_One_Parent_Generation;
    begin
@@ -343,12 +387,17 @@ end Is_Father;
          Mother : T_Arbre_Personnes;
          Count : Integer := 0;
       begin 
+      -- R0 - Get the orphans
+         -- R1 - Check for ancestors
          if not Is_Null (Tree) then
+            -- R2 - Add the ancestor if it is an orphan
             if Is_Orphan (Tree) then
                Ancestors(1) := Get_Value(Tree);
             else
+            -- R2 - Get the ancestors of the mother and the father
                Mother := Get_Mother(Tree);
                Father := Get_Father(Tree);
+               -- R3 - Get the orphan nodes in the mother tree 
                if not Is_Null (Mother) then 
                   Mother_Ancestors := Nodes_Without_Parent_Generation(Mother, Depth-1);
                   for i in Mother_Ancestors'Range loop 
@@ -356,6 +405,7 @@ end Is_Father;
                      Count := Count + 1;
                   end loop;
                end if;
+               -- R3 - Get the orphan nodes in the father tree 
                if not Is_Null (Father) then
                   Father_Ancestors := Nodes_Without_Parent_Generation(Father, Depth-1);
                   for i in Father_Ancestors'Range loop
@@ -364,6 +414,7 @@ end Is_Father;
                end if;
             end if;
          end if;
+         -- R1 - Return the ancestors
          return Ancestors;
       end Nodes_Without_Parent_Generation;
    begin
